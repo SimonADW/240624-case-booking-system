e023702de450375e5d023924e7b1fd78c5bddd0c
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-06-24 15:29:50 +0200

    Set form to controlled component

--- src/Components/BookingForm/BookingForm.jsx
+++ src/Components/BookingForm/BookingForm.jsx
@@ -4,43 +4,58 @@ import style from "./BookingForm.module.css";
 
 const BookingForm = () => {
 	const [searchResult, setSearchResult] = useState([]);
-  return (
-	<>
-		<form className={style.bookingForm} action="">
-			<div>
-				<label htmlFor="firstname">First Name</label>
-				<input type="text" id="firstname"/>
-			</div>
-			<div>
-				<label htmlFor="lastname">Last Name</label>
-				<input type="text" id="lastname"/>
-			</div>
-			<div>
-				<label htmlFor="arrivaldate">Arrival Date</label>
-				<input type="date" id="arrivaldate" />
-			</div>
-			<div>
-				<label htmlFor="nights">Nights</label>
-				<input type="number" id="nights" defaultValue={1}/>
-			</div>
-			<div>
-				<label htmlFor="category">Room Category</label>
-				<select name="category" id="category">
-					<option value="singleroom1pax">Single Room, 1 person</option>
-					<option value="doubleroom2pax">Double Room, 2 persons</option>
-					<option value="doubleroom1pax">Double Room, 1 person</option>					
-				</select>
-			</div>
-			<div>
-				<label htmlFor="roomnumber">Room</label>
-				<input type="num" id="roomnumber" defaultValue={100}/>
-			</div>
+	const [formValues, setFormValues] = useState({
+		arrivalDate: "",
+		nights: 1,
+		category: "",
+		roomnum: "",
+		firstname: "",
+		lastname: "",
+	});
 
-		</form>
+	return (
+		<>
+			<form className={style.bookingForm} action="">
+				<div>
+					<label htmlFor="arrivaldate">Arrival Date</label>
+					<input type="date" id="arrivaldate" value={formValues.arrivalDate} />
+				</div>
+				<div>
+					<label htmlFor="nights">Nights</label>
+					<input type="number" id="nights" value={formValues.nights} />
+				</div>
+				<div>
+					<label htmlFor="category">Room Category</label>
+					<select name="category" id="category">
+						<option value="singleroom1pax" selected>
+							Single Room, 1 person
+						</option>
+						<option value="doubleroom2pax">
+							Double Room, 2 persons
+						</option>
+						<option value="doubleroom1pax">
+							Double Room, 1 person
+						</option>
+					</select>
+				</div>
+				<div>
+					<label htmlFor="roomnumber">Room</label>
+					<input type="num" id="roomnumber" value={formValues.roomnum} />
+				</div>
+				<div>
+					<label htmlFor="firstname">First Name</label>
+					<input type="text" id="firstname" value={formValues.firstname} />
+				</div>
+				<div>
+					<label htmlFor="lastname">Last Name</label>
+					<input type="text" id="lastname" value={formValues.lastname} />
+				</div>
+				<button>Search availability</button>
+			</form>
 
-		{/* <BookingSearchResults searchResult={searchResult} /> */}
-	</>
-  )
-}
+			{/* <BookingSearchResults searchResult={searchResult} /> */}
+		</>
+	);
+};
 
-export default BookingForm
+export default BookingForm;
